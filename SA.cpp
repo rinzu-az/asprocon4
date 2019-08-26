0fb7c499a4e024835fd87b2e55f223b17d7f1ce4
rinzu-az <fuwa.lincle@gmail.com>
2019-08-27 05:24:33 +0900

    [modify/fix]バグ修正/評価関数追加

--- SA.cpp
+++ SA.cpp
@@ -14,7 +14,8 @@ typedef pair<ll,ll> Pll;
 #define len(X) ((int)(X).size())
 #define fi first
 #define sc second
-
+const int MAX_TIME = 86400000;
+const ll P_MAX = 1e10;
 clock_t start = clock();
 
 #ifdef LOCAL
@@ -44,28 +45,135 @@ struct Order {
   int a;
   Order (int _r, int _i, int _e, int _d, int _q, int _pr, int _a) :
     r(_r), i(_i), e(_e), d(_d), q(_q), pr(_pr), a(_a) {}
-  
-
-  int eval(){
-    return 0;
-  }
-  Order neighbor(Order _order){
-    return _order;
-  }
 };
 
 struct Bom {
-  vector<int> mList;
-  vector<int> sList;
+  vector<Pii> infos;
 };
 
 struct Operation {
-  int r;
-  int m;
-  int t1;
-  int t2;
-  int t3;
+  int r, m, t1, t2, t3;
+  Operation (int _r, int _m, int _t1, int _t2, int _t3) :
+    r(_r), m(_m), t1(_t1), t2(_t2), t3(_t3) {}
 };
+
+ll eval(vector<Operation> operations){
+    for (int n = 0; n < R; ++n) {
+      Operation ope = operations[n];
+      int r,m,t1,t2,t3;
+      tie(r, m, t1, t2, t3) = make_tuple(ope.r, ope.m, ope.t1, ope.t2, ope.t3);
+
+      if (r < 0 || r >= R) {
+        cerr << "r is out of range" << endl;
+        return -1;
+      }
+      if (m < 0 || m >= M) {
+        cerr << "m is out of range" << endl;
+        return -1;
+      }
+      if (t1 < 0 || t1 > MAX_TIME) {
+        cerr << "t1 is out of range" << endl;
+        return -1;
+      }
+      if (t2 < 0 || t2 > MAX_TIME) {
+        cerr << "t2 is out of range" << endl;
+        return -1;
+      }
+      if (t3 <= 0 || t3 > MAX_TIME) {
+        cerr << "t3 is out of range" << endl;
+        return -1;
+      }
+      operations.push_back(Operation(r, m, t1, t2, t3));
+    }
+
+    set<int> orderSet;
+    for (int n = 0; n < R; ++n) {
+      if (orderSet.count(operations[n].r) == 1) {
+        cerr << "分割禁止 : Split operation detected" << endl;
+        return 1;
+      }
+      orderSet.insert(operations[n].r);
+    }
+
+    sort(all(operations), [](const Operation& o1, const Operation& o2) {
+          if (o1.m != o2.m) {
+            return o1.m < o2.m;
+          } else {
+            return o1.t1 < o2.t1;
+          }
+        });
+
+
+    vector<int> mToPreviousT3(M, 0);
+    vector<int> mToPreviousI(M, -1);
+
+    // 作業ごとの制約チェック
+    for (int n = 0; n < R; ++n) {
+      Operation& ope = operations[n];
+      Order& order = orders[ope.r];
+      Bom& bom = boms[order.i];
+      int m = ope.m;
+      int e = (mToPreviousI[m] < 0) ? order.e : max(order.e, mToPreviousT3[m]);
+      bool canAssign = false;
+      int mIndex = 0;
+      for (int j = 0; j < len(bom.infos); ++j) {
+        if (bom.infos[j].first == m) {
+          canAssign = true;
+          mIndex = j;
+          break;
+        }
+      }
+      if (!canAssign) {
+        cerr << "BOM違反 : BOM violation" << endl;
+        return 1;
+      }
+      if (e > ope.t1) {
+        cerr << "製造開始時刻違反 : Scheduling start time violation" << endl;
+        return 1;
+      }
+      if (ope.t1 > ope.t2 || ope.t2 > ope.t3) {
+        cerr << "段取り開始時刻、製造開始時刻と製造終了時刻順番違反: t1 <= t2 && t2 <= t3" << endl;
+        return 1;
+      }
+      if (mToPreviousI[m] >= 0) {
+        int setupT = setupTimes[m][Pii(mToPreviousI[m], order.i)];
+        if ((ope.t2 - ope.t1) != setupT) {
+          cerr << "段取り時間間違い: Invalid Setup Time" << endl;
+          return 1;
+        }
+      } else {
+        if ((ope.t2 - ope.t1) != 0) {
+          cerr << "段取り時間間違い: Invalid Setup Time" << endl;
+          return 1;
+        }
+      }
+      int manuT = order.q * bom.infos[mIndex].second;
+      if ((ope.t3 - ope.t2) != manuT) {
+        cerr << "製造時間間違い : Manufacturing time error" << endl;
+        return 1;
+      }
+
+      mToPreviousI[m] = order.i;
+      mToPreviousT3[m] = ope.t3;
+    } // operation loop end
+
+    // 評価
+    ll V1 = 0, V2 = 0;
+    for (int n = 0; n < R; ++n) {
+      Operation& ope = operations[n];
+      Order& order = orders[ope.r];
+      V1 += ope.t2 - ope.t1;
+      if (ope.t3 <= order.d) {
+        V2 += order.pr;
+      } else {
+        ll delay = ope.t3 - order.d;
+        V2 += order.pr - ceil((double)order.pr * delay / order.a);
+      }
+    }
+
+    ll P = P_MAX - V1 + V2;
+    return P;
+}
 
 int M, I, R, BL, CL;
 vector<Order> orders;
@@ -80,8 +188,7 @@ void readProblem() {
   for (int n = 0; n < BL; ++n) {
     int i, m, s;
     cin >> _str >> i >> m >> s;
-    boms[i].mList.push_back(m);
-    boms[i].sList.push_back(s);
+    boms[i].infos.push_back(Pii(m,s));
   }
   setupTimes.resize(M);
   for (int n = 0; n < CL; ++n) {
@@ -105,7 +212,9 @@ void writeSolution() {
 
 void solve() {
   stable_sort(all(orders), [](const Order& o1, const Order& o2) { return o1.e < o2.e; });
-
+  for(auto bom : boms){
+    sort(all(bom.infos), [](const Pii& info1, const Pii& info2){ return info1.second > info2.second; });
+  }
   vector<int> mToPreviousT3(M);
   vector<int> mToPreviousI(M, -1);
 
@@ -114,8 +223,8 @@ void solve() {
     Order& order = orders[n];
     Bom& bom = boms[order.i];
     Operation& ope = operations[n];
-    int m = bom.mList[0];
-    int s = bom.sList[0];
+    int m, s;
+    tie(m,s) = bom.infos[0];
     ope.r = order.r;
     ope.m = m;
     if (mToPreviousI[m] < 0) {
@@ -130,8 +239,6 @@ void solve() {
     mToPreviousT3[m] = ope.t3;
   }
 }
-
-
 
 template<class T>
 class simulated_annealing{
@@ -154,8 +261,8 @@ class simulated_annealing{
   public:
   T exec(){
     for(int i=0; i<10000; i++){
-      T newAns = T.neighbor();
-      int newScore = T.eval();
+      T newAns = ans.neighbor();
+      int newScore = newAns.eval();
       if(accept() || newScore > score){
         tie(ans,score) = make_pair(newAns, newScore);
       }

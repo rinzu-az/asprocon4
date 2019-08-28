aece715c02a9c64e5672ab74a60c9e888b200535
rinzu-az <fuwa.lincle@gmail.com>
2019-08-29 07:58:19 +0900

    [modify]evalの高速化を試行

--- SA.cpp
+++ SA.cpp
@@ -69,7 +69,6 @@ struct Operation {
     r(_r), m(_m), t1(_t1), t2(_t2), t3(_t3) {}
 };
 
-
 int M, I, R, BL, CL;
 vector<Order> orders;
 vector<Bom> boms;
@@ -138,11 +137,52 @@ vector<Operation> toOperations(vector<vector<int>> ans) {
   return opes;
 }
 
-vector<vector<int>> neighbor(vector<vector<int>> ans, bool flg = false){
+ll eval(vector<vector<int>> ans){
+  vector<Operation> opes;
+  ll V1 = 0, V2 = 0;
+  for(int i=0; i<M; i++){
+    int t1 = 0, prev = -1;
+    vector<int> machine = ans[i];
+    for(int j=0; j<len(machine); j++){
+      int n = machine[j];
+      Order& order = orders[n];
+      Bom& bom = boms[order.i];
+      Operation ope;
+      int s = 1;
+      for(auto b : bom.infos){
+        if(b.first == i){
+          s = b.second; break;
+        }
+      }
+      ope.r = order.r;
+      ope.m = i;
+      if (prev < 0) {
+        ope.t1 = order.e;
+        ope.t2 = ope.t1;
+      } else {
+        ope.t1 = max(order.e, t1);
+        ope.t2 = ope.t1 + setupTimes[i][Pii(prev, order.i)];
+      }
+      ope.t3 = ope.t2 + order.q * s;
+      t1 = ope.t3; prev = order.i;
+
+      V1 += ope.t2 - ope.t1;    if (ope.t3 <= order.d) {
+        V2 += order.pr;
+      } else {
+        ll delay = ope.t3 - order.d;
+        V2 += order.pr - ceil((double)order.pr * delay / order.a);
+      }
+    }
+  }
+  ll P = P_MAX - V1 + V2;
+  return P;
+}
+
+vector<vector<int>> neighbor(vector<vector<int>> ans, bool flg = true){
   int m = rand(0,M-1);
   int from = rand(0,len(ans[m])-1);
   int n = m;
-  if(flg ? true : chance(100)) {
+  if(flg ? true : chance(80)) {
     vector<Pii> bominfo = boms[orders[ans[m][from]].i].infos;
     n = bominfo[rand(0,len(bominfo)-1)].first;
   }
@@ -156,24 +196,17 @@ vector<vector<int>> neighbor(vector<vector<int>> ans, bool flg = false){
   return ans;
 }
 
-ll eval(vector<Operation> operations){
-  // 評価
-  ll V1 = 0, V2 = 0;
-  for (int n = 0; n < R; ++n) {
-    Operation& ope = operations[n];
-    Order& order = orders[ope.r];
-    V1 += ope.t2 - ope.t1;
-    if (ope.t3 <= order.d) {
-      V2 += order.pr;
-    } else {
-      ll delay = ope.t3 - order.d;
-      V2 += order.pr - ceil((double)order.pr * delay / order.a);
-    }
-  }
+vector<vector<int>> swapnext(vector<vector<int>> ans){
+  int m = rand(0,M-1);
+  int idx = rand(0,len(ans[m])-2);
 
-  ll P = P_MAX - V1 + V2;
-  return P;
+  if(ans[m].size() > 1) swap(ans[m][idx], ans[m][idx+1]);
+
+  if(DEBUG) cout << m << " " << idx << endl;
+  return ans;
 }
+
+
 
 vector<vector<int>> shuffle(vector<vector<int>>& ans, int i){
   int m = rand(0,M-1);
@@ -207,13 +240,15 @@ class simulated_annealing{
   simulated_annealing(format _ans) :
     ans(_ans) {}
   
-  format exec(double timeLimit){
+  format exec(double timeLimit, int type = 0){
     bool flg = true;
     while(get_time() < timeLimit){
       //if(flg && get_time() > 7.0) flg = false;
       for(int i=0; i<100; i++){
-        format newAns = neighbor(ans, flg);
-        ll newScore = eval(toOperations(newAns));
+        format newAns;// = neighbor(ans, flg);
+        if(type == 0) newAns = neighbor(ans, flg);
+        if(type == 1) newAns = swapnext(ans);
+        ll newScore = eval(newAns);
         if(DEBUG) cout << "newScore : " << newScore << " Score : " <<  score << endl;
 
         if((newScore > 0 && accept()) || newScore > score){
@@ -237,14 +272,11 @@ class beam_search{
   ll bestScore = 0, score = 0;
   int cnt = 0;
 
-  //function
-
-
   //constructor
   public:
   beam_search() {}
   beam_search(format _ans){
-    beams.insert(make_pair(eval(toOperations(_ans)), _ans));
+    beams.insert(make_pair(eval(_ans), _ans));
   }
   
   vector<vector<int>> exec(double timeLimit){
@@ -252,7 +284,7 @@ class beam_search{
       multimap<ll, format> newBeams = beams;
       for(auto beam : beams){
         format newAns = neighbor(beam.second);
-        ll newScore = eval(toOperations(newAns));
+        ll newScore = eval(newAns);
         if(DEBUG) cout << "newScore : " << newScore << " Score : " <<  score << endl;
 
         newBeams.insert(make_pair(newScore, newAns));
@@ -266,7 +298,6 @@ class beam_search{
     return (*beams.rbegin()).second;
   }
 };
-
 
 void solve() {
   vector<Order> _orders = orders;
@@ -310,17 +341,18 @@ void solve() {
     prev[m] = Pii(ope.t3, order.i);
     ans[m].push_back(order.r);
   }
-  if(M == 1){
+  if(M == 1 && I <= 5){
     auto best = ans;
-    for(int i=0; i<19; i++){
+    for(int i=0; i<10; i++){
       simulated_annealing SA(ans);
-      auto tmp = SA.exec((i+1) * 0.5);
-      if(eval(toOperations(tmp)) > eval(toOperations(best))) best = tmp;
+      auto res = SA.exec((i+1) * 0.5);
+      if(eval(res) > eval(best)) best = res;
       ans = best;
       for(int j=0; j<5; j++) shuffle(ans, j);
     }
     simulated_annealing SA(best);
     ans = SA.exec(TIME_LIMIT);
+    //ans = SA.exec(TIME_LIMIT, 1);
   }else if(M == 0){
     beam_search BS(ans); 
     ans = BS.exec(TIME_LIMIT);
@@ -330,48 +362,6 @@ void solve() {
   }
   operations = toOperations(ans);
 }
-
-void Xsolve() {
-  stable_sort(all(orders), [](const Order& o1, const Order& o2) { return o1.d < o2.d; });
-  for(auto bom : boms){
-    sort(all(bom.infos), [](const Pii& info1, const Pii& info2){ return info1.second > info2.second; });
-  }
-
-  int hoge = M==1 ? 2e7 : 0;
-  vector<Pii> prev(M,Pii(hoge,-1)); //t3, i
-
-  operations.resize(R);
-  for (int n = 0; n < R; ++n) {
-    Order& order = orders[n];
-    Bom& bom = boms[order.i];
-    Operation& ope = operations[order.r];
-    int m, s, mint3;
-    Pii info = bom.infos[0];
-    tie(m,s) = info;
-    mint3 = prev[info.first].first;
-
-    for(int j=0; j<len(bom.infos); j++){
-      Pii info = bom.infos[j];
-      int prevt3 = prev[info.first].first;
-      if(mint3 > prevt3){
-        mint3 = prevt3;
-        tie(m,s) = bom.infos[j];
-      }
-    }
-    ope.r = order.r;
-    ope.m = m;
-    if (prev[m].second < 0) {
-      ope.t1 = max(order.e, prev[m].first);
-      ope.t2 = ope.t1;
-    } else {
-      ope.t1 = max(order.e, prev[m].first);
-      ope.t2 = ope.t1 + setupTimes[m][Pii(prev[m].second, order.i)];
-    }
-    ope.t3 = ope.t2 + order.q * s;
-    prev[m] = Pii(ope.t3, order.i);
-  }
-}
-
 
 int main() {
   readProblem();
